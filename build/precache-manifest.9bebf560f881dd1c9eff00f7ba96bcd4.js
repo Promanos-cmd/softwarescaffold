c240ceeef13deef902ef76f4d0190a6477b28fba
Promanos-cmd <64768987+Promanos-cmd@users.noreply.github.com>
2022-05-14 09:45:33 +1000

    Add files via upload

---
+++ build/precache-manifest.9bebf560f881dd1c9eff00f7ba96bcd4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f61280c2e807c461d41e7f0e785d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42c9689902763b31ac4",
+    "url": "/static/css/main.99e75adc.chunk.css"
+  },
+  {
+    "revision": "b9ed94f790aa178dbb48",
+    "url": "/static/js/2.83ff5494.chunk.js"
+  },
+  {
+    "revision": "914a85462fc888825cb3a498c2b9869c",
+    "url": "/static/js/2.83ff5494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42c9689902763b31ac4",
+    "url": "/static/js/main.abb51aed.chunk.js"
+  },
+  {
+    "revision": "cc454f581cdc072df54e",
+    "url": "/static/js/runtime-main.4c381e45.js"
+  },
+  {
+    "revision": "715313cd4f8b7b826663309b8751bb6b",
+    "url": "/static/media/svg-1.715313cd.svg"
+  },
+  {
+    "revision": "4a122f73bc839268339ab652e3ec374d",
+    "url": "/static/media/svg-2.4a122f73.svg"
+  },
+  {
+    "revision": "d9e440a5ed9283a035d51b4cb93663f0",
+    "url": "/static/media/svg-3.d9e440a5.svg"
+  },
+  {
+    "revision": "494b2abdfb54672b38d5c6135a33eda5",
+    "url": "/static/media/svg-5.494b2abd.svg"
+  },
+  {
+    "revision": "72ed571599b33a2d2810daa85d57e890",
+    "url": "/static/media/video.72ed5715.mp4"
+  }
+]);
